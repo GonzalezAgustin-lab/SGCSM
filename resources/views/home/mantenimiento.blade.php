feeb0f121b37386dd577f710e5880b0d07e06d0f
Lafedar <agusgonzalez603@hotmail.com>
2024-08-26 01:16:13 -0300

    En el index se agrega icono para acceder a mantenimiento preventivo

--- resources/views/home/mantenimiento.blade.php
+++ resources/views/home/mantenimiento.blade.php
@@ -6,23 +6,28 @@
     <br><br><br>
   
     <div class="row">
-        <div class="col-md-12"></div>
-           <br><br>
-             <div class="col-md-4">
-                <a  href="/solicitudes"> <img  src="{{ URL::to('/img/solicitudes.png') }}" height="140"></a>
-                <h2 style="color: #3b557a">Solicitudes</h2>
-            </div>
-
-            <div class="col-md-4">
-                <a  href="/equipos_mant"> <img  src="{{ URL::to('/img/equipos.png') }}" height="140"></a>
-                <h2 style="color: #3b557a">Equipos</h2>
-            </div>
-    
-            <div class="col-md-4">
-                <a  href="/parametros_mantenimiento"> <img  src="{{ URL::to('/img/parametros.png') }}" height="140"></a>
-                <h2 style="color: #3b557a">Parametros</h2>
-            </div>
+        <br><br>
+        <div class="col-md-6 text-center">
+            <br>
+            <a href="/solicitudes"> <img  src="{{ URL::to('/img/solicitudes.png') }}" height="140"></a>
+            <h2 style="color: #3b557a">Solicitudes</h2>
         </div>
+        <div class="col-md-6 text-center">
+            <br>
+            <a href="/equipos_mant"> <img  src="{{ URL::to('/img/equipos.png') }}" height="140"></a>
+            <h2 style="color: #3b557a">Equipos</h2>
+        </div>
+        <div class="col-md-6 text-center">
+            <br>
+            <a href="/mantenimientoPreventivo"> <img  src="{{ URL::to('/img/mantenimientoPreventivo.png') }}" height="140"></a>
+            <h2 style="color: #3b557a">Mantenimiento Preventivo</h2>
+        </div>
+        <div class="col-md-6 text-center">
+            <br>
+            <a href="/parametros_mantenimiento"> <img  src="{{ URL::to('/img/parametros.png') }}" height="140"></a>
+            <h2 style="color: #3b557a">Parametros</h2>
+        </div>
+
     </div>
 </div>
 <div id="footer-lafedar"></div>
